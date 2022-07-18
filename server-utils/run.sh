9c8b5c73238bcf0edc20db5e47e144e23c31cb3b
jalallinux <smjjalalzadeh93@gmail.com>
2022-07-18 15:33:47 +0430

    work on run.sh

--- server-utils/run.sh
+++ server-utils/run.sh
@@ -2,8 +2,8 @@ rm -rf source && mkdir source
 tar -xzvf source.tar.gz -C ./source
 cp .env source && cd source
 
-docker stop $(docker-compose ps | grep hidevs_ | awk '{print $1}')
-docker rm $(docker-compose ps | grep hidevs_ | awk '{print $1}')
+docker stop $(docker ps -a | grep hidevs_ | awk '{print $1}')
+docker rm $(docker ps -a | grep hidevs_ | awk '{print $1}')
 docker volume rm $(docker volume ls --filter name=hidevs_ -q)
 
 tar -xzvf app.tar.gz -C /
