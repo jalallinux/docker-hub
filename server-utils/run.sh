06787cb1cc0e55f7a679e03c920f44ce146b77bf
jalallinux <smjjalalzadeh93@gmail.com>
2022-07-18 15:19:37 +0430

    work on structure

--- server-utils/run.sh
+++ server-utils/run.sh
@@ -1,7 +1,12 @@
+mkdir source
+tar -xzvf source.tar.gz -C ./source
+cd source
+
 docker stop $(docker-compose ps | grep hidevs_ | awk '{print $1}')
 docker rm $(docker-compose ps | grep hidevs_ | awk '{print $1}')
 docker volume rm $(docker volume ls --filter name=hidevs_ -q)
 
 tar -xzvf app.tar.gz -C /
 docker-compose up --build --force-recreate -d
-rm -rf $(pwd)
+
+cd .. && rm -rf $(pwd)
